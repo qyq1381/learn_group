954911360b38778965e1fcd51800257c4a53f9fb
qyq1381 <qyq1381@gmail.com>
2018-07-10 15:09:39 -0500

    added development environment

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var open = require('open');
+
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+
+app.get('/',function(req, res){
+    res.sendFile('c:/Users/qyq13/git/learn_group/index.html')
+});
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+});
