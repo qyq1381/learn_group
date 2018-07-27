ade60409f63e91b08c1baf2c581f618b05885f06
sonedigo <sonedigo@gmail.com>
2018-07-27 16:28:31 -0500

    Fully Form function without submit & real data based on Lihong's work

--- src/js/form.js
+++ src/js/form.js
@@ -2,15 +2,29 @@ window.onload = function () {
     let zip = document.getElementById("zipcode");
     let year = document.getElementById("Yearid");
     let make1 = document.getElementById("Makeid1");
+    let make2 = document.getElementById("Makeid2");
+    let make3 = document.getElementById("Makeid3");
     let make1obj = { m1: "abc", m2: "bcd" };
-    create_year_options(year, 2018, 2010);
+    let make2obj = { m1: "2_abc", m2: "2_bcd"};
+    let make3obj = {m1:"3_abc", m2: "3_bcd"};
+    create_year_options(year, 2018, 2017);
     create_make1_options(make1, make1obj);
+    create_make2_options(make2, make2obj);
+    create_make3_options(make3, make3obj);
     make1.disabled = true;
+    make2.disabled =true;
+    make3.disabled = true;
     zip.oninput = function () {
         check("zipcode");
     }
     year.onchange = function () {
         make1.disabled = false;
+    }
+    make1.onchange = function(){
+        make2.disabled = false;
+    }
+    make2.onchange = function(){
+        make3.disabled = false;
     }
 };
 function check(x) {
@@ -37,3 +51,15 @@ function create_make1_options(make1, make1obj) {
     option = new Option(make1obj.m2, make1obj.m2, false, false);
     make1.appendChild(option);
 };
+function create_make2_options(make2, make2obj){
+    let option = new Option(make2obj.m1, make2obj.m1, false, false);
+    make2.appendChild(option);
+    option = new Option(make2obj.m2, make2obj.m2, false, false);
+    make2.appendChild(option);
+}
+function create_make3_options(make3, make3obj){
+    let option = new Option(make3obj.m1, make3obj.m1, false, false);
+    make3.appendChild(option);
+    option = new Option(make3obj.m2, make3obj.m2, false, false);
+    make3.appendChild(option);
+}
